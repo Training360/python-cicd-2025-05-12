6f6af6527837537b35a5d10b2199f74be8909c7d
Istvan Viczian <viczian.istvan@gmail.com>
2025-05-12 13:33:44 +0200

    Unit test

---
+++ simple-calculator/test_calculator.py
@@ -0,0 +1,15 @@
+from calculator import add
+
+
+def test_add():
+    # Given
+    a = 3
+    b = 5
+    # When
+    result = add(a, b)
+    # Then
+    assert result == 8
+
+
+def test_add_short():
+    assert add(3, 5) == 8
